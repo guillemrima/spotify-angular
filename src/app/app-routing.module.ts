9c513af7a5bee1a2e42c048fe725cbd5395cb762
guillemrima <117720260+guillemrima@users.noreply.github.com>
2023-10-27 18:35:13 +0200

    Feat: App Scaffolding & defined path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  }
 ];
 
 @NgModule({
